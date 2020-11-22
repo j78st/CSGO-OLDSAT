9ed84c3059267cc80bc69a21219e8246d6e6a024
jhSaleh <jsaleh@enssat.fr>
2020-11-22 11:09:39 +0100

    ebaucheMusique

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,29 @@
-import  demo.*;
-public class Main {
-    public static void main(String[] args) {
-        Money myAccount = new Money(42, "EUR");
 
-        System.out.println(myAccount.toString());
+import Music.Systems.WorldBoxDisc;
+
+public class Main {
+    public static void sleep(int n){
+        try{
+            Thread.sleep(n);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        WorldBoxDisc.init(); //Initialise toutes les pistes
+        sleep(3000); //Temps d'attente pour le chargement des musiques
+
+        //Plusieurs sons peuvent être joué en même temps
+        WorldBoxDisc.play("wind"); //Système qui appelle les musiques selon un nom unique au format String
+        WorldBoxDisc.play("opinel");
+        WorldBoxDisc.play("porteDebloque");
+        WorldBoxDisc.getVolume("wind");
+        sleep(1000);
+        WorldBoxDisc.setVolume("wind", 10);
+        sleep(1000);
+        WorldBoxDisc.setVolume("porteDebloque", 15);
+        WorldBoxDisc.showLoadedAudioFiles();
+        while(true);
     }
 }
