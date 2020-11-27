62aa80e96051a9afa8da571ed81cebc2f894dcb0
jhSaleh <jsaleh@enssat.fr>
2020-11-27 19:01:11 +0100

    modif

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,5 @@ import  demo.*;
 public class Main {
     public static void main(String[] args) {
         Money myAccount = new Money(42, "EUR");
-
-        System.out.println(myAccount.toString());
     }
 }
